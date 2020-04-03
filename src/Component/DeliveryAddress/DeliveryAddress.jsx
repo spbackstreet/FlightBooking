f8c9fc7103a15630f02cd86443e19dae8606789f
karan1.shah <karan1.shah@ril.com>
2020-04-03 16:36:05 +0530

    Design changes as per discussion

--- src/Component/DeliveryAddress/DeliveryAddress.jsx
+++ src/Component/DeliveryAddress/DeliveryAddress.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { FixedHeader } from '../../commom/FixedHeader';
 import Spinner from 'react-spinner-material';
 import OtpDialogue from '../OtpDialogue/OtpDialogue';
-
+import '../../css/style.css';
 
 const display = {
     display: 'block'
@@ -45,60 +45,66 @@ const DeliveryAddress = () => {
                                                             <div class="login">
                                                                 <div class="form-group">
                                                              
-                                                                    <span class="remove-no"> <img class="img-fluid" src="./img/pos/icon-remove.png" width="16px" height="16px" onClick={ (e) => setMsdn('')} /></span>
-                                                                    <input id="customerName"  type="text" required="required"  name="customerName"     autocomplete="off" placeholder=" "  class="jio-form-control"
+                                                                    {/* <span class="remove-no"> <img class="img-fluid" src="./img/pos/icon-remove.png" width="16px" height="16px" onClick={ (e) => setMsdn('')} /></span> */}
+
+
+                                                                    <label style={{ color: "black", "fontWeight": "bolder",marginBottom:"0px"}}>House No/Flat No/Building/Apartment<label style={{ color: "#FF0000" }}>*</label></label>
+                                                                    <input id="customerName"  type="text" required="required"  name="customerName"     autocomplete="off" placeholder=" " 
+                                                                    style={{ width: "100%", padding: "12px 20px", margin: "8px 0",display: "inline-block", border: "1px solid #ccc", "border-radius": "4px", "box-sizing": "border-box",border: "2px solid rgb(13, 149, 162)","border-radius": "8px"}} 
                                                                   //  onChange = { (e) => updateMsdn(e)}
                                                                     //onChange={(e) =>this.validateMobile(e.target.value)} value={msdn}
                                                                     />
-                                                                     <label for="customerName" class="control-label">House No/Flat No/Building/Apartment<label style={{ color: "#FF0000" }}>*</label></label>
+                                                                     {/* <label for="customerName" class="control-label">House No/Flat No/Building/Apartment<label style={{ color: "#FF0000" }}>*</label></label> */}
                                                                 </div>
 
 
 
 
                                                                 <div class="form-group">
-                                                                    <span class="remove-no"> <img class="img-fluid" src="./img/pos/icon-remove.png" width="16px" height="16px" onClick={ (e) => setMsdn('')} /></span>
-                                                                    <input id="landMark"  type="text" required="required"  name="landMark"     autocomplete="off" class="jio-form-control" placeholder=" "
+                                                                    <label style={{ color: "black", "fontWeight": "bolder", marginBottom:"0px"}}>Landmark<label style={{ color: "#FF0000" }}>*</label></label>
+                                                                    <input id="landMark"  type="text" required="required"  name="landMark"     autocomplete="off"  style={{ width: "100%", padding: "12px 20px", margin: "8px 0",display: "inline-block", border: "1px solid #ccc", "border-radius": "4px", "box-sizing": "border-box",border: "2px solid rgb(13, 149, 162)","border-radius": "8px"}} placeholder=" "
                                                                   //  onChange = { (e) => updateMsdn(e)}
                                                                     //onChange={(e) =>this.validateMobile(e.target.value)} value={msdn}
                                                                     />
-                                                                    <label for="customerName" class="control-label">Landmark<label style={{ color: "#FF0000" }}>*</label></label>
+                                                                    {/* <label for="customerName" class="control-label">Landmark<label style={{ color: "#FF0000" }}>*</label></label> */}
                                                                 </div>
 
 
                                                                 <div class="form-group">
-                                                                    <span class="remove-no"> <img class="img-fluid" src="./img/pos/icon-remove.png" width="16px" height="16px" onClick={ (e) => setMsdn('')} /></span>
-                                                                    <input id="roadName"  type="text" required="required"  name="roadName"     autocomplete="off" class="jio-form-control" placeholder=" "
+                                                                    <label style={{ color: "black", "fontWeight": "bolder",marginBottom:"0px" }}>Street Address/Road Name <label style={{ color: "#FF0000" }}>*</label></label>
+                                                                    <input id="roadName"  type="text" required="required"  name="roadName"     autocomplete="off"  style={{ width: "100%", padding: "12px 20px", margin: "8px 0",display: "inline-block", border: "1px solid #ccc", "border-radius": "4px", "box-sizing": "border-box",border: "2px solid rgb(13, 149, 162)","border-radius": "8px"}} placeholder=" "
                                                                   //  onChange = { (e) => updateMsdn(e)}
                                                                     //onChange={(e) =>this.validateMobile(e.target.value)} value={msdn}
                                                                     />
-                                                                    <label for="customerName" class="control-label">Street Address/Road Name <label style={{ color: "#FF0000" }}>*</label></label>
+                                                                    {/* <label for="customerName" class="control-label">Street Address/Road Name <label style={{ color: "#FF0000" }}>*</label></label> */}
                                                                 </div>
 
 
                                                                 <div class="form-group">
-                                                                    <span class="remove-no"> <img class="img-fluid" src="./img/pos/icon-remove.png" width="16px" height="16px" onClick={ (e) => setMsdn('')} /></span>
-                                                                    <input id="area"  type="text" required="required"  name="area"   autocomplete="off" class="jio-form-control" placeholder=" "
+                                                                    {/* <span class="remove-no"> <img class="img-fluid" src="./img/pos/icon-remove.png" width="16px" height="16px" onClick={ (e) => setMsdn('')} /></span> */}
+                                                                    <label style={{ color: "black", "fontWeight": "bolder", marginBottom:"0px" }}>Area/Sector/Locality<label style={{ color: "#FF0000" }}>*</label></label>
+
+                                                                    <input id="area"  type="text" required="required"  name="area"   autocomplete="off" style={{ width: "100%", padding: "12px 20px", margin: "8px 0",display: "inline-block", border: "1px solid #ccc", "border-radius": "4px", "box-sizing": "border-box",border: "2px solid rgb(13, 149, 162)","border-radius": "8px"}} placeholder=" "
                                                                   //  onChange = { (e) => updateMsdn(e)}
                                                                     //onChange={(e) =>this.validateMobile(e.target.value)} value={msdn}
                                                                     />
-                                                                    <label for="customerName" class="control-label">Area/Sector/Locality<label style={{ color: "#FF0000" }}>*</label></label>
+                                                                    {/* <label for="customerName" class="control-label">Area/Sector/Locality<label style={{ color: "#FF0000" }}>*</label></label> */}
                                                                 </div>
 
 
                                                                 <div class="form-group">
-                                                                    <span class="remove-no"> <img class="img-fluid" src="./img/pos/icon-remove.png" width="16px" height="16px" onClick={ (e) => setMsdn('')} /></span>
-                                                                    <input id="pinCode"  type="number" required="required"  name="pinCode"  autocomplete="off" class="jio-form-control" placeholder=" "
+                                                                <label style={{ color: "black", "fontWeight": "bolder", marginBottom:"0px" }}>Pincode<label style={{ color: "#FF0000" }}>*</label></label>
+                                                              <input id="pinCode"  type="number" required="required"  name="pinCode"  autocomplete="off" style={{ width: "100%", padding: "12px 20px", margin: "8px 0",display: "inline-block", border: "1px solid #ccc", "border-radius": "4px", "box-sizing": "border-box",border: "2px solid rgb(13, 149, 162)","border-radius": "8px"}}  placeholder=" "
                                                                   //  onChange = { (e) => updateMsdn(e)}
                                                                     //onChange={(e) =>this.validateMobile(e.target.value)} value={msdn}
                                                                     />
-                                                                    <label for="customerName" class="control-label">Pincode<label style={{ color: "#FF0000" }}>*</label></label>
+                                                                   
                                                                 </div>
 
 
                                                                 <div class="form-group">
-                                                                    <span class="remove-no"> <img class="img-fluid" src="./img/pos/icon-remove.png" width="16px" height="16px" onClick={ (e) => setMsdn('')} /></span>
-                                                                    <select id="village"  type="number" required="required"  name="village"  autocomplete="off" class="jio-form-control" placeholder=" "
+                                                                <label style={{ color: "black", "fontWeight": "bolder", marginBottom:"0px" }}>Village/Town/City<label style={{ color: "#FF0000" }}>*</label></label>
+                                                                    <select id="village"  type="number" required="required"  name="village"  autocomplete="off" style={{ width: "100%", padding: "12px 20px", margin: "8px 0",display: "inline-block", border: "1px solid #ccc", "border-radius": "4px", "box-sizing": "border-box",border: "2px solid rgb(13, 149, 162)","border-radius": "8px"}}  placeholder=" "
                                                                   //  onChange = { (e) => updateMsdn(e)}
                                                                     //onChange={(e) =>this.validateMobile(e.target.value)} value={msdn}
                                                                     >
@@ -107,30 +113,29 @@ const DeliveryAddress = () => {
                                                                         <option>Village 2</option>
 
                                                                         </select>
-                                                                    <label for="customerName" class="control-label">Village/Town/City<label style={{ color: "#FF0000" }}>*</label></label>
                                                                 </div>
 
                                                                 <div class="form-group">
-                                                                    <span class="remove-no"> <img class="img-fluid" src="./img/pos/icon-remove.png" width="16px" height="16px" onClick={ (e) => setMsdn('')} /></span>
-                                                                    <select id="district"  type="number" required="required"  name="district"  autocomplete="off" class="jio-form-control" placeholder=" "
+                                                                <label style={{ color: "black", "fontWeight": "bolder", marginBottom:"0px" }}>District<label style={{ color: "#FF0000" }}>*</label></label>
+                                                                    <select id="district"  type="number" required="required"  name="district"  autocomplete="off" style={{ width: "100%", padding: "12px 20px", margin: "8px 0",display: "inline-block", border: "1px solid #ccc", "border-radius": "4px", "box-sizing": "border-box",border: "2px solid rgb(13, 149, 162)","border-radius": "8px"}}  placeholder=" "
                                                                   //  onChange = { (e) => updateMsdn(e)}
                                                                     //onChange={(e) =>this.validateMobile(e.target.value)} value={msdn}
                                                                     >
                                                                         <option></option>
                                                                         <option>District 1</option>
                                                                         </select>
-                                                                    <label for="customerName" class="control-label">District<label style={{ color: "#FF0000" }}>*</label></label>
                                                                 </div>
 
 
 
                                                                 <div class="form-group">
-                                                                    <span class="remove-no"> <img class="img-fluid" src="./img/pos/icon-remove.png" width="16px" height="16px" onClick={ (e) => setMsdn('')} /></span>
-                                                                    <input id="state"  type="text" required="required"  name="state"  autocomplete="off" class="jio-form-control" placeholder=" "
+                                                                <label style={{ color: "black", "fontWeight": "bolder", marginBottom:"0px" }}>State<label style={{ color: "#FF0000" }}>*</label></label>
+                                                  
+                                                                 <input id="state"  type="text" required="required"  name="state"  autocomplete="off" style={{ width: "100%", padding: "12px 20px", margin: "8px 0",display: "inline-block", border: "1px solid #ccc", "border-radius": "4px", "box-sizing": "border-box",border: "2px solid rgb(13, 149, 162)","border-radius": "8px"}} placeholder=" "
                                                                   //  onChange = { (e) => updateMsdn(e)}
                                                                     //onChange={(e) =>this.validateMobile(e.target.value)} value={msdn}
                                                                     />
-                                                                    <label for="customerName" class="control-label">State<label style={{ color: "#FF0000" }}>*</label></label>
+                                                                    {/* <label for="customerName" class="control-label">State<label style={{ color: "#FF0000" }}>*</label></label> */}
                                                                 </div>
 
 
