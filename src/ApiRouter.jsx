d0b2717a2d56395d78060361ba02258d174ef275
cc <=>
2020-04-06 17:28:38 +0530

    refactored api router

--- src/ApiRouter.jsx
+++ src/ApiRouter.jsx
@@ -1,216 +1,157 @@
 import axios from 'axios';
-import React, { Component } from "react";
-import { async, reject } from 'q';
-import { resolve } from 'path';
-import { Encrypt, decryptData, EncryptNew, encryption, Decryption, encryptHeader, encryptionDataWithRandomKey } from '../../Common/JS/Encryption-Decryption';
-import config from '../../config';
-import { conditionalExpression } from '@babel/types';
-// import * as constants from './constants';
-import { TIMEOUT } from 'dns';
+import { basicAuth } from './basicAuth';
+import config from '../config';
+
+// *************** For API Call **********************
+const postApiCall = async (Request, ApiName) => {
+  try {
+    return await axios.post(ApiName,
+      Request)
+  }
+  catch (err) {
+    console.log("err.message : ", err);
+    return "1000";
+  }
+}
 
 
-class APIRouter extends React.Component {
+const createHeaderStoreID = () => {
 
-  //  *************** For New Encryption ***********//
-  async postApiCalNewEncryption(Request, ApiName) {
-
-    var encryptedData = encryptionDataWithRandomKey(Request,  config.userID, config.objDeviceSave.Msg);
-    var keyHeader = encryptHeader(Request,// constants.publickey, 
-        config.userID, encryptedData);
-    //var newHeader=this.createHeaderForNewEncryption(keyHeader);
-    try {
-
-      const options = {
-        headers: this.createHeaderForNewEncryption(keyHeader)
-      };
-      return await axios.post(ApiName,
-        encodeURIComponent(encryptedData.toString()),
-        options,
-        { timeout:options ///changes needed
-            //constants.apiTimeOut 
-        }).then(Response => {
-
-          return decryptData(Response.data.replace(/"/g, ""));
-        }).catch(err => {
-
-          return "1000";
-        })
-    }
-    catch (e) {
-      return "1000";
-    }
+  if (config.applicationType == 'React') {
+    var basicAuthVal = basicAuth(config.userID);
+    basicAuthVal = 'Basic ' + basicAuthVal
+  }
+  else {
+    basicAuthVal = config.basicAuth
   }
 
-  createHeaderForNewEncryption(header) {
-    var rquestHeader = {
-      Accept: 'application/json',
-      'Content-Type': 'text/plain',
-      Authorization: config.basicAuth,
-      GUID: config.objDeviceSave.Msg,
-      storeid: config.objSupervisorLogin.Circle_Response.org,
-      KEY: header,
-      USERID: config.userID
-    }
-
-    return rquestHeader;
-  }
-  // *********************** END *****************//
-
-
-  //added by cc
-
-  async postApiCall(request, ApiName) {
-    var EncryptedRequest = Encrypt(request);
-    try {
-      const options = {
-        headers: this.createHeaderAgentAuth()
-      };
-      return await axios.post(config[config.Environment].BaseUrl + ApiName,
-        EncryptedRequest,
-        options,
-        { timeout: options, }).then(Response => {
-
-          return decryptData(Response.data.replace(/"/g, ""));
-        }).catch(err => {
-
-          return "1000";
-        })
-
-    }
-    catch{
-      return "1000";
-    }
+  var rquestHeader = {
+    Accept: 'application/json',
+    'Content-Type': 'text/plain',
+    authorization: basicAuthVal,
+    GUID: config.guid,
+    storeid: config.storeID,
+    USERID: config.userID
 
   }
 
-  async postApiCal(request, ApiName) {
-    var EncryptedRequest = Encrypt(request);
-    try {
-      const options = {
-        headers: this.createHeader()
-      };
-      return await axios.post(ApiName,
-        EncryptedRequest,
-        options,
-        { timeout: options, }).then(Response => {
-
-          return decryptData(Response.data.replace(/"/g, ""));
-        }).catch(err => {
-
-          return "1000";
-        })
-    }
-    catch{
-      return "1000";
-    }
-
-
-  }
-
-
-  createHeaderAgentAuth() {
-    var rquestHeader = {
-      Accept: 'application/json',
-      'Content-Type': 'text/plain',
-      authorization: EncryptNew(config.basicAuth),
-      userid: EncryptNew(config.userID)
-
-    }
-    return rquestHeader;
-  }
-
-  createHeaderL() {
-
-    var rquestHeader = {
-      Accept: 'application/json',
-      'Content-Type': 'text/plain',
-      authorization: config.basicAuth,
-      GUID: config.objDeviceSave.Msg,
-      // 'X-API-Key': config.objSupervisorLogin.APIKey,
-      // storeid: config.objGetStore.StoreID
-    }
-
-    return rquestHeader;
-  }
-
-  createHeader() {
-
-    var rquestHeader = {
-      Accept: 'application/json',
-      'Content-Type': 'text/plain',
-      authorization: config.basicAuth,
-      GUID: config.objDeviceSave.Msg,
-      storeid: config.objSupervisorLogin.Circle_Response.org
-    }
-    return rquestHeader;
-  }
-
-
-  async postApiCalStoreID(request, ApiName) {
-    var EncryptedRequest = Encrypt(request);
-
-    try {
-
-      const options = {
-        headers: this.createHeaderStoreID()
-      };
-      return await axios.post(ApiName,
-        EncryptedRequest,
-        options,
-        { timeout: options, })
-        .then(Response => {
-
-          return decryptData(Response.data.replace(/"/g, ""));
-        }).catch(err => {
-
-          return "1000";
-        })
-    }
-    catch (err) {
-      return "1000";
-    }
-  }
-
-  async postApiCalStoreIDJava(request, ApiName) {
-    var EncryptedRequest = Encrypt(request);
-
-    try {
-
-      const options = {
-        headers: this.createHeaderStoreID()
-      };
-      return await axios.post(ApiName,
-        EncryptedRequest,
-        options,
-        { timeout: options, })
-        .then(Response => {
-
-          return decryptData(Response.data.replace(/"/g, ""));
-        }).catch(err => {
-
-          return "1000";
-        })
-    }
-    catch (e) {
-      return "1000";
-    }
-
-  }
-
-  createHeaderStoreID() {
-
-    var rquestHeader = {
-      Accept: 'application/json',
-      'Content-Type': 'text/plain',
-      authorization: config.basicAuth,
-      GUID: config.objDeviceSave.Msg,
-      storeid: config.objSupervisorLogin.Circle_Response.org
-    }
-
-    return rquestHeader;
-
-  }
+  return rquestHeader;
 
 }
-export default (APIRouter);
 
+
+
+// ***************** For New Enc Headers
+const createHeaderForNewEncryption = (header, basicAuth) => {
+  var rquestHeader = {
+    Accept: 'application/json',
+    'Content-Type': 'text/plain',
+    Authorization: basicAuth,
+    GUID: config.guid,
+    storeid: config.storeID,
+    KEY: header,
+    USERID: config.userID
+  }
+
+  return rquestHeader;
+}
+
+
+
+
+// ***************** For .Net **************
+
+const postApiCalStoreIDDotNet = (EncryptedRequest, ApiName) => {
+  try {
+    const options = {
+      headers: createHeaderStoreIDDotNet()
+    };
+    return axios.post(ApiName,
+      EncryptedRequest,
+      options)
+
+
+  }
+  catch (err) {
+    return "1000";
+  }
+}
+
+
+
+
+const createHeaderStoreIDDotNet = () => {
+
+  if (config.applicationType == 'React') {
+    var basicAuthVal = basicAuth(config.userID);
+    basicAuthVal = 'Basic ' + basicAuthVal
+  }
+  else {
+    basicAuthVal = config.basicAuth
+  }
+
+  var rquestHeader = {
+    Accept: 'application/json',
+    'Content-Type': 'text/plain',
+    authorization: basicAuthVal,
+    GUID: config.guid,
+    USERID: config.userID
+
+  }
+
+  return rquestHeader;
+
+}
+
+
+
+
+
+//******************** For Devfin
+const postApiCalDevfin = (Request, ApiName) => {
+  console.log("header devfin : ", createHeaderDevfin())
+  try {
+    const options = {
+      headers: createHeaderDevfin()
+    };
+    return axios.post(ApiName,
+      Request,
+      options)
+
+
+  }
+  catch (err) {
+    return "1000";
+  }
+}
+
+
+
+
+const createHeaderDevfin = () => {
+
+  if (config.applicationType == 'React') {
+    var basicAuthVal = basicAuth(config.userIDDevfin);
+    basicAuthVal = 'Basic ' + basicAuthVal
+  }
+  else {
+    basicAuthVal = 'Basic TyFtVSRlcjpQQHNzdzByZA=='
+
+  }
+
+  var rquestHeader = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    authorization: 'Basic TyFtVSRlcjpQQHNzdzByZA==',
+
+  }
+  return rquestHeader;
+}
+
+
+
+
+export {
+  postApiCall, postApiCalStoreIDDotNet, postApiCalDevfin, createHeaderDevfin, createHeaderForNewEncryption
+  , createHeaderStoreIDDotNet, createHeaderStoreID
+};
