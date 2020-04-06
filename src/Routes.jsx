1f67754f5a070b0c434c34e0e535e1b9878355d6
karan1.shah <karan1.shah@ril.com>
2020-04-06 10:47:00 +0530

    addition of local ref page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,6 +3,8 @@ import { Route, Switch, Redirect } from "react-router-dom";
 const Home = React.lazy(() => import('./Component/Home/Home'));
 const DeliveryAddress = React.lazy(() => import('./Component/DeliveryAddress/DeliveryAddress'));
 const PermanentAddress= React.lazy(() => import('./Component/PermanentAddress/PermanentAddress'));
+const LocalReference= React.lazy(() => import('./Component/LocalRef/LocalReference'));
+
 // const LocalAddress = React.lazy(() => import('./components/LocalAddress/LocalAddress'));
 
 
@@ -14,7 +16,7 @@ const Routes = () => {
             <Route exact path="/" component={Home} />
             <Route exact path="/deliveryAddress" component={DeliveryAddress} />
             <Route exact path="/permanentAddress" component={PermanentAddress} />
-           
+            <Route exact path="/localreference" component={LocalReference} />
             <Redirect to='/' />
         </Switch>
     );
